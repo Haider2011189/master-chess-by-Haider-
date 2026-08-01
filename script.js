25fb23df6d48ec98a0fe94cf8985f237d293b84b
Haider2011189 <HASAN.NAQVI78@GMAIL.COM>
2026-08-01 11:33:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,59 @@ var pieceSymbols = {
   P: '♙', R: '♖', N: '♘', B: '♗', Q: '♕', K: '♔'
 };
 
+// Explicit Wikimedia SVG mapping to guarantee pieces load without CORS blocks
+function customPieceTheme (piece) {
+  var pieces = {
+    'wP': 'https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg',
+    'wR': 'https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg',
+    'wN': 'https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg',
+    'wB': 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg',
+    'wQ': 'https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg',
+    'wK': 'https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg',
+    'bP': 'https://upload.wikimedia.org/wikipedia/commons/c/c7/Chess_pdt45.svg',
+    'bR': 'https://upload.wikimedia.org/wikipedia/commons/ff/ff/Chess_rdt45.svg',
+    'bN': 'https://upload.wikimedia.org/wikipedia/commons/f/f1/Chess_ndt45.svg',
+    'bB': 'https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg',
+    'bQ': 'https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg',
+    'bK': 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg'
+  };
+  return pieces[piece];
+}
+
+// --- HIGHLIGHTING LOGIC ---
+function removeHighlights () {
+  $('#myBoard .square-55d63').removeClass('highlight-square');
+}
+
+function highlightSquare (square) {
+  var $square = $('#myBoard .square-' + square);
+  $square.addClass('highlight-square');
+}
+
+function onMouseoverSquare (square, piece) {
+  // Get list of possible moves for this square
+  var moves = game.moves({
+    square: square,
+    verbose: true
+  });
+
+  // Exit if there are no moves available for this square
+  if (moves.length === 0) return;
+
+  // Highlight the square being hovered
+  highlightSquare(square);
+
+  // Highlight all possible destination squares
+  for (var i = 0; i < moves.length; i++) {
+    highlightSquare(moves[i].to);
+  }
+}
+
+function onMouseoutSquare (square, piece) {
+  removeHighlights();
+}
+
+// --- GAMEPLAY LOGIC ---
 function onDragStart (source, piece, position, orientation) {
   if (game.game_over()) return false;
 
@@ -20,6 +73,8 @@ function onDragStart (source, piece, position, orientation) {
 }
 
 function onDrop (source, target) {
+  removeHighlights();
+
   // Check if move is legal
   var move = game.move({
     from: source,
@@ -65,23 +120,23 @@ function updateCaptured() {
     if (move.captured) {
       var symbol = pieceSymbols[move.captured] || move.captured;
       if (move.color === 'w') {
-        // White captured a black piece
         $capturedWhite.append('<span>' + symbol + '</span>');
       } else {
-        // Black captured a white piece
         $capturedBlack.append('<span>' + symbol + '</span>');
       }
     }
   });
 }
 
+// Board Configuration
 var config = {
   draggable: true,
   position: 'start',
-  // HIGH-RES WORKING PIECE THEME (Lichess Cesta Set):
-  pieceTheme: 'https://raw.githubusercontent.com/lichess-org/lila/master/public/piece/cesta/{piece}.svg',
+  pieceTheme: customPieceTheme,
   onDragStart: onDragStart,
   onDrop: onDrop,
+  onMouseoverSquare: onMouseoverSquare,
+  onMouseoutSquare: onMouseoutSquare,
   onSnapEnd: onSnapEnd
 };
 
